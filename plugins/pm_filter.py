6c73d9e5cbefbf360b37f440e66127d2ccf9b967
100nuk <146798173+100nuk@users.noreply.github.com>
2024-01-17 15:58:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1047,7 +1047,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
                   ]
                   ]
                   reply_markup = InlineKeyboardMarkup(buttons)
-                  ms = await query.edit_message_text(f"**chat successful disabled** ✅\n\n**chat ID**: {chatID}
                  \n\n**chat Title**:{chatTitle}", reply_markup=reply_markup)
              except Exception as e:
                   ms.edit(f"Got a Lazy error:\n{e}" )
